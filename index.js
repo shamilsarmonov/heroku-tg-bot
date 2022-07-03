01042731545659608eb77d313bee91d1520acc2b
Shamil <shamil.sarmonov@nu.edu.kz>
2022-07-03 16:50:18 +0600

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '5578421862:AAH0LFxvhbL_Nepnpb-2fHlHRJvCbZKOEgs'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"`)
+})
